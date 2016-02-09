c12df72164389f060439057494ed1e893d0a53c4
Luda Shu <ludashu.88@hotmail.com>
2016-02-09 01:19:14 -0800

    updated homepage and added pages and css

--- application/views/homepage.php
+++ application/views/homepage.php
@@ -1,4 +1,11 @@
-<div class="row">
+<div class="row" id="stocksPanel">
+    {stockportfolios}
+    <h5>STOCKS</h5>
+    <div class="span4"><a href="{href}"><img src="/data/{mug}" title="{who}"/></a></div>
+    {/stockportfolios}
+</div> 
+<br/><hr/><br/>
+<div class="row" id="playersPanel">
     {portfolios}
     <div class="span4"><a href="{href}"><img src="/data/{mug}" title="{who}"/></a></div>
     {/portfolios}
